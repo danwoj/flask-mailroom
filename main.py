7baa2845b599595c232a5cbeb4e5a3fd8f2b9b6b
danwoj <32502388+danwoj@users.noreply.github.com>
2018-04-17 18:37:15 -0700

    Week #1 Assignment
    I submitted this in the last quarter's Git repository. Instructions unclear where to commit assignments so I'm doing it here as well.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
 
-from model import Donation 
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -15,9 +15,14 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+@app.route('/donations/<name>')
+def donations_by_name(name):
+#    return 'Hello, {}!'.format(name)
+    donations = Donation.select().join(Donor).where(Donor.name == name)
+#    donations = Donation.select().where(Donor.name == 'Alice')
+    return render_template('donations.jinja2', donations=donations)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
